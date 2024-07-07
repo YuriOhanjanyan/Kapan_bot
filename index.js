dd39f3702421f65ae182ee3573d963108da93bba
YuriOhanjanyan <yuri.ohanjanyan@tumo.org>
2024-07-07 15:04:31 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const TelegramBot = require('node-telegram-bot-api')
+const express = require('express')
 require('dotenv').config()
 const token = process.env.TOKEN
+
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 const bot = new TelegramBot(token, { polling: true })
 const commands = [
