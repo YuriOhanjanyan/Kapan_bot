d951a2e1459ca9cda46aed8ab2ccca501573ec13
YuriOhanjanyan <yuri.ohanjanyan@tumo.org>
2024-07-07 15:02:52 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ bot.on('text', (msg) => {
 
     bot.sendVideo(chatId, './video.mp4')
   }
-   else if (msg.text === '/photos') {
+  else if (msg.text === '/photos') {
     bot.sendMessage(chatId, "Start photo uploading...")
     bot.sendPhoto(chatId, './Kapan.jpg')
     bot.sendMessage(chatId, "[Ավելին](https://hy.wikipedia.org/wiki/%D4%BF%D5%A1%D5%BA%D5%A1%D5%B6#%D5%8A%D5%A1%D5%BF%D5%AF%D5%A5%D6%80%D5%A1%D5%BD%D6%80%D5%A1%D5%B0)", {
@@ -72,8 +72,8 @@ bot.on('text', (msg) => {
     })
   } else if (msg.text === '/khustup_mountain') {
     bot.sendMessage(chatId, "Start photo uploading...")
-    bot.sendPhoto(chatId, './Khustup.jpg', )
-    bot.sendPhoto(chatId, './Khustup1.jpeg', )
+    bot.sendPhoto(chatId, './Khustup.jpg',)
+    bot.sendPhoto(chatId, './Khustup1.jpeg',)
     bot.sendMessage(chatId, "[Խուստուփ լեռ](https://hy.wikipedia.org/wiki/%D4%BD%D5%B8%D6%82%D5%BD%D5%BF%D5%B8%D6%82%D6%83)", {
       parse_mode: "MarkdownV2",
       disable_web_page_preview: true,
@@ -86,11 +86,11 @@ bot.on('text', (msg) => {
     bot.sendMessage(chatId, "Վահանավանք նաև Հովհաննավանք, միջնադարյան վանական համալիր Հայաստանում։ Գտնվում է Սյունիքի մարզկենտրոն Կապան քաղաքից 7 կմ արևմուտք՝ Ողջի գետի աջ կողմում, Տիգրանասարի լանջին։ Վանական համալիրի առաջին կառույցը՝ սուրբ Գրիգոր Լուսավորիչ եկեղեցին, կառուցվել է 911 թվականին՝ Տաթևի վանքի հանդիսավոր օծումից հինգ տարի անց։ Կառուցումը հովանավորել է Սյունիք նահանգի Բաղք գավառի իշխան Ձագիկի որդի Վահան Սյունին, ով եկեղեցուն նվիրաբերում է հսկայական կալվածքներ։ Ավելի ուշ՝ 11-րդ դարի երկրորդ կեսին, Վահանավանքի մոտ կատարվում են նոր կառուցումներ, մեծ եկեղեցուց 25 մետր հեռու կառուցվում է մի ավելի փոքր եկեղեցի՝ սուրբ Աստվածածինը։ Նրա արևելյան պատի ներսի կողմում, հիմքին մոտ գտնվող քարերից մեկի վրա պահպանվել է 1086 թվականին վերաբերվող մի արձանագրություն, որտեղ խոսվում է այդ եկեղեցու կառուցման մասին։ Վանքն ունեցել է շուրջ 100 միաբան, կից գործել է վանական դպրոցը, որի սաներից էր եղել եկեղեցու հիմնադիր Վահանի եղբորորդի Վահան Ա Սյունեցին։ Վահանավանքում են գտնվում Վահան իշխանի, Սահակ Սևադա Բ-ի, Սոփի թագուհու, Սյունիքի գահերեց իշխան Աշոտի, նրա թագակիր որդիներ Սմբատ Բ-ի և Գրիգոր Ա-ի, ինչպես նաև Սյունիքի թագավորության մնացած չորս թագավորների, Վաչագան իշխանի մայր Խաշուշի գերեզմանները։ 1966 թվականից կատարվում են պեղումներ, իսկ 1978 թվականից՝ վերականգնողական աշխատանքներ։ 2006-2009 թվականներին իրականացված վերականգնման աշխատանքների արդյունքում ամբողջովին վերականգնվել են սուրբ Գրիգոր Լուսավորիչ եկեղեցին և նրան կից գավիթը։ Սյունասրահը մասնակիորեն է վերականգնվել։ Համալիրի երկհարկ եկեղեցին")
     bot.sendPhoto(chatId, './Vahanavank.jpeg')
     bot.sendPhoto(chatId, './Vahanavank1.jpeg')
+    bot.sendMessage(chatId, "[Վահանավանքի կոորդինատներ](https://maps.app.goo.gl/r9hM8YRXk3CFPBqf8)", {
+      parse_mode: "MarkdownV2",
+      disable_web_page_preview: true,
+    })
   }
-  bot.sendMessage(chatId, "[Վահանավանքի կոորդինատներ](https://maps.app.goo.gl/r9hM8YRXk3CFPBqf8)", {
-    parse_mode: "MarkdownV2",
-    disable_web_page_preview: true,
-  })
-}) 
+})
 
 
